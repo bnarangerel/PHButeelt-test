139051160a67814536d23cffe18109ee8eadc137
narangerel <naraa.hikari1214@gmail.com>
2022-02-18 15:47:43 +0800

    hi gahai min

---
+++ Program hangamj buteelt/src/jishee1/main.java
@@ -0,0 +1,7 @@
+package jishee1;
+
+public class main {
+	public static void main(String[] args) {
+		System.out.println("bi bugdiin martcij ahhaa");
+	}
+}
---
+++ odoYgAlna/src/odoYgAlna/jishee.java
@@ -0,0 +1,7 @@
+package odoYgAlna;
+
+public class jishee {
+	public static void main(String[] args) {
+		System.out.println("bi bugdiin martcij ahhaa");
+	}
+}
