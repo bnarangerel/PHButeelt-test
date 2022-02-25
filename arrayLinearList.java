cf15723e77a804d1c5e4dfc89d69a1d59ae931d1
bnarangerel <44729806+bnarangerel@users.noreply.github.com>
2022-02-25 14:23:49 +0800

    Update arrayLinearList.java

--- arrayLinearList.java
+++ arrayLinearList.java
@@ -6,6 +6,11 @@ public class arrayLinearList {
 	private int size;
 	
 	public arrayLinearList(int hemjee) {
-		
+		if(hemjee >= 0){
+			element = new Object [hemjee];
+		}
+	}
+	public arrayLinearList(){
+		this(10);
 	}
 }
